b88b2e3777750643df7819a2061e0d4e72e2b303
SkyHeads <skyheads@live.com>
2021-06-03 00:09:19 -0300

    setup miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return {
+        transactions: [
+          {
+            id: 1,
+            title: 'Aplicação Web com ReactJS',
+            type: 'deposit',
+            category: 'jobs',
+            amount: 6000,
+            createdAt: new Date('2021-02-12 09:00:00'),
+          },
+          {
+            id: 2,
+            title: 'Aluguel',
+            type: 'withdraw',
+            category: 'house',
+            amount: 800,
+            createdAt: new Date('2021-03-01 10:00:00'),
+          },
+        ],
+      };
+    });
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
