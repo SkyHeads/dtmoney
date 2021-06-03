cc81598296a051254406fa76f8ee2743fef92c11
SkyHeads <skyheads@live.com>
2021-06-03 01:57:20 -0300

    setup react modal in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export const App: React.FC = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
