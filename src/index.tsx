8d420ff07ce2dd6f947e4209fd9310c693a61c7d
SkyHeads <skyheads@live.com>
2021-06-04 03:13:00 -0300

    create post transaction route && model transaction

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return {
-        transactions: [
-          {
-            id: 1,
-            title: 'Aplicação Web com ReactJS',
-            type: 'deposit',
-            category: 'jobs',
-            amount: 6000,
-            createdAt: new Date('2021-02-12 09:00:00'),
-          },
-          {
-            id: 2,
-            title: 'Aluguel',
-            type: 'withdraw',
-            category: 'house',
-            amount: 800,
-            createdAt: new Date('2021-03-01 10:00:00'),
-          },
-        ],
-      };
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     });
   },
 });
